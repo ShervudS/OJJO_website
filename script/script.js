08b98f6ab0ede61c28b46dbbbfe6b583b4ddc6a8
theCount <thescheremetev@mail.ru>
2021-01-14 00:07:19 +0300

    I add Header and burger icon

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
 'use strict'
+const bgMenu = document.querySelector('.burger__menu'),
+      bgLine = bgMenu.querySelector('.burger__menu--line');
+
+bgMenu.addEventListener('click', () => {
+   bgLine.classList.toggle('activ'); 
+});

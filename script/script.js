5864ee85ad86a737a02f7003fba503e1984f89a9
theCount <thescheremetev@mail.ru>
2021-02-04 14:08:35 +0300

    I do catalog page

--- script/script.js
+++ script/script.js
@@ -1,9 +1,16 @@
 'use strict'
 const bgMenu = document.querySelector('.burger__menu'),
-      bgLine = bgMenu.querySelector('.burger__menu--line');
+      bgLine = bgMenu.querySelector('.burger__menu--line'),
+      catalogBtnShowCards = document.querySelector('.catalog__show-more'),
+      catalogCards = document.querySelector('.catalog__cards.hidden');
 
 bgMenu.addEventListener('click', () => {
    bgLine.classList.toggle('activ'); 
+});
+
+catalogBtnShowCards.addEventListener('click', () => {
+   catalogCards.classList.remove('hidden');
+   catalogBtnShowCards.classList.add('hidden');
 });
 
 // const ahref = document.querySelectorAll('a');
